6d41475c0df9e6fe4946fac463cf25dff230de7c
beming-dev <mingfordev@gmail.com>
2024-12-05 23:01:29 +0900

    Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(process.env.PORT!) || 3002, // 환경 변수가 없으면 기본값으로 3000 사용
-  },
+  base: "./", // 상대 경로로 설정
 });
