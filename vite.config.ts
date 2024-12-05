bb773720953eae9d536beed3a7119368e48dbd55
beming-dev <mingfordev@gmail.com>
2024-12-05 12:14:50 +0900

    Add dynamic port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT || "3000"), // 환경 변수가 없으면 기본값으로 3000 사용
+  },
+});
